4bef15876aa444325d93583a2e90805c7b48cf81
amitabhisgood <amitabhisgood@gmail.com>
2024-02-21 21:07:48 +0530

    Create LeftComponent.js

---
+++ src/components/LeftComponent.js
@@ -0,0 +1,16 @@
+function LeftComponent() {
+  // You can customize the list of items as needed
+  const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  
+  return (
+    <div>
+      <ul style={{ listStyleType: 'none' }}>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default LeftComponent;
